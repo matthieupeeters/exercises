275fc73080e6250e41b1409f0920b83d16e2a86d
matthieupeeters <matthieupeeters@users.noreply.github.com>
2015-10-21 09:59:23 +0200

    Add a "find_first_match" function
    The "find_first_match" can make the handling of 2 arrays a little clearer.

--- list_exercise.php
+++ list_exercise.php
@@ -33,6 +33,7 @@ array(
 996,
 1031,
 1060,
+1066,
 1108,
 1137,
 1180);
@@ -62,13 +63,43 @@ function my_reverse(array $a) {
   return $rv;
   
 }
-
 // Note that neither function uses the php-library. For this exercise it is not allowed to use it.
 
 // However, it is allowed to use it for displaying results, in this case with echo and print_r:
 
 echo my_max($a1);
 print_r(my_reverse($a2));
+
+
+
+// Finds the smallest value that is present in two *sorted* arrays
+function find_first_match(array $a, array $b) {
+  $i = 0;
+  $j = 0;
+  while($j < count($a) && $j < count($b) && $a[$i] != $b[$j]) { // Note that the check whether $i and $j are in range...
+    // ...has to come before whe use $a[$i] or $b[$j]. 
+    // Also note that when we check something in the condition of a while-statement, it is true in the whole body,
+    // unless the checked values are changed. 
+    if($a[$i] < $b[$j]) {
+      $i++;  // The sought value has to be bigger than $a[$i], and thus come after $a[$i]
+    } else { // $a[$i] > $b[$j] since where are in a while loop that demands $a[$i] != $b[$j]
+      $j++;  // The sought value has to be bigger than $b[$j]. 
+    }
+  }
+  if ($j < count($a) && $j < count($b)) { // This means that the condition on which the while-loop ended was $a[$i] != $b[$j]..
+    // ..That means that $a[$i] == $b[$i] and we have found our equal value. We don't check whether $a[$i] == $b[$j] because..
+    // ..if $i >= count($a) or $j >= count($b) we would try to address values that are not part of the array. Resulting in..
+    // ..unpredictable behaviour.
+    return $a[$i];
+  } else {
+    throw new Exception('Value not found.');    
+  }
+}
+
+
+echo find_first_match($a1, $a2);
+
+
 
 // Task: Design a function that takes two array-parameters containing sorted lists and merges them into one array,
 // in a way that results into a sorted array containing all elements of the original two lists.
